d1ede8bfd2ffefabc0be2c6eb2a4eae587140329
maxim1317 <snesarev1317@gmail.com>
2018-03-06 20:11:45 +0300

    ?

--- Fluid/src/fluid.cpp
+++ Fluid/src/fluid.cpp
@@ -7,7 +7,7 @@ int main(int argc, char const *argv[])
     deltaX = 0.1;
     maxT = 1.8;
     maxX = 1.8;
-    bool flag = 0;
+    bool flag = 1;
     init(deltaT, deltaX, maxT, maxX, flag);
     return 0;
 }
